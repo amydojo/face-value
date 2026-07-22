56f53abe104890fe879fb549ef5ac9a38471b1c5
amydojo <amycdo98@gmail.com>
2026-07-22 16:47:19 -0700

    feat: activate canonical parity layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { FaceValueProvider } from './app/FaceValueProvider';
 import { AppRouter } from './app/router/AppRouter';
 import './styles/foundations.css';
 import './styles/canonical.css';
+import './styles/parity.css';
 import './styles/motion.css';
 
 createRoot(document.getElementById('root')!).render(
