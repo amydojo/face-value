3994002b1e7bae4d8b6fcad486a7b540f4d84a08
amydojo <amycdo98@gmail.com>
2026-07-22 17:19:07 -0700

    fix: activate stage focus origin guard

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { FaceValueProvider } from './app/FaceValueProvider';
+import { StageFocusManager } from './app/StageFocusManager';
 import { AppRouter } from './app/router/AppRouter';
 import './styles/foundations.css';
 import './styles/canonical.css';
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <FaceValueProvider>
+        <StageFocusManager />
         <AppRouter />
       </FaceValueProvider>
     </BrowserRouter>
