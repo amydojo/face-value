7eb0e45f3a6bca46ab1e0f8ea15ab64b7c99b7d2
amydojo <amycdo98@gmail.com>
2026-07-24 13:22:54 -0700

    feat: route direct verdict deep link

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { FaceValueApplication } from '../../features/FaceValueApplication';
+import { VerdictRoute } from '../../features/verdict/VerdictRoute';
 
 export function AppRouter() {
   return (
     <Routes>
+      <Route path="/verdict" element={<VerdictRoute />} />
       <Route path="*" element={<FaceValueApplication />} />
     </Routes>
   );
