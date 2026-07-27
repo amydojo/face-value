3738e3232cd26f29e75b67c383a946d09704d900
amydojo <amycdo98@gmail.com>
2026-07-27 13:01:29 -0700

    feat: expose protected calibration route

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { HumanButterProductionJourney } from '../../features/HumanButterProductionJourney';
 import { EvidenceMachineDemo } from '../../features/evidence-machine/EvidenceMachineDemo';
+import { YouCamCalibration } from '../../features/youcam-calibration/YouCamCalibration';
 import { YouCamSpike } from '../../features/youcam-spike/YouCamSpike';
 
 export function AppRouter() {
@@ -8,6 +9,7 @@ export function AppRouter() {
     <Routes>
       <Route path="/evidence-machine" element={<EvidenceMachineDemo />} />
       <Route path="/youcam-spike" element={<YouCamSpike />} />
+      <Route path="/youcam-calibration" element={<YouCamCalibration />} />
       <Route path="*" element={<HumanButterProductionJourney />} />
     </Routes>
   );
